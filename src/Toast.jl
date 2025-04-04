8d54f38588a39b6cb3f8942bb268e96dfe1d623a
Nick Stone <nicholasestone@proton.me>
2025-04-04 14:51:55 -0500

    bumper now working

--- src/Toast.jl
+++ src/Toast.jl
@@ -2,6 +2,120 @@ module Toast
 
 using Hexagons, Delica, Tessen, Unitful, Statistics
 import Base:convert
+import Tessen:HatchLine, pointalong, intersections
+
+"""
+```julia
+createconfig([filename])
+```
+Write an example config file to `filename`. If `filename` is omitted, the file
+will be written to `"config.jl"`.
+
+# Configuration Parameters
+- dfield: calibrated FOV for the objective being used
+- hbottom: distance from the bottom of the post to the bottom of the beams
+- htop: distance from the bottom of the beams to the top of the bumpers
+- chamferbottom: angle at which the bottoms of the posts should be chamfered
+- chamfertop: angle at which the tops of the posts and beams should be chamfered
+- cutangle: angles at which blocks should be cut to avoid shadowing
+- overlap: amount that neighboring blocks should overlap to ensure they are connected
+- nhammock: number of hammock slices to write
+- dhammockslice: distance between hammock slices
+- dslice: slicing distance
+- wbumper: width of the bumpers
+- fillet: fillet radius on xz and yz crossections of the posts and bumpers
+- wpost: post width
+- hbeam: beam height
+- lbeammax: maximum total beam length
+- maxseglength: maximum length of a single beam segment
+- keygap: closest point between the two halves of a beam before they are closed with a keystone
+- dhammockhatch: hammock hatching distance
+- dhatch: hatching distance for posts, beams and bumpers
+- laserpower: laser power for posts, beams and bumpers
+- scanspeed: scan speed for posts, beams and bumpers
+- stagespeed: max stage speed
+- interfacepos: how far into the substrate writing should begin
+- hamscanspeed: scan speed for hammocks
+- hamlaserpower: laser power for hammocks
+"""
+function createconfig(filename="config.jl")
+    config = """Dict(
+        :dfield => 1750u"µm",
+        :hbottom => 30u"µm",
+        :htop => 70u"µm",
+        :chamferbottom => 0,
+        :chamfertop => pi/6,
+        :cutangle => pi/6,
+        :overlap => 10u"µm",
+        :dslice => 1u"µm",
+        :nhammock => 5,
+        :dhammockslice => 280u"nm",
+        :wbumper => 200u"µm",
+        :fillet => 20u"µm",
+        :wpost => 50u"µm",
+        :hbeam => 10u"µm",
+        :lbeammax => 150u"µm",
+        :maxseglength => 30u"µm",
+        :keygap => 20u"µm",
+        :dhammockhatch => 1u"µm",
+        :dhatch => 300u"nm",
+        :laserpower => 50u"mW",
+        :scanspeed => 50_000u"µm/s",
+        :stagespeed => 100u"µm/s",
+        :interfacepos => 10u"µm",
+        :hamscanspeed => 2_000u"µm/s",
+        :hamlaserpower => 50u"mW"
+    )
+    """
+    open(filename,"w") do io
+        print(io,config)
+    end
+end
+
+function bumperedgecoords(;kwargs...)
+    #copy out the variables we want
+    hbottom = kwargs[:hbottom]
+    htop = kwargs[:htop]
+    chamfertop = kwargs[:chamfertop]
+    chamferbottom = kwargs[:chamferbottom]
+    wbumper = kwargs[:wbumper]
+    fillet = kwargs[:fillet]
+    #figuring out how to fillet everything in crossection seems hard enough that I'm just going
+    #to use Tessen hatching machinery to do it for me.
+    #this function will return a closure which gives the bottom and top coordinates of a bumper
+    #as a function of z position
+
+    #draw our crossection on the xz plane
+    verts=[
+        [-wbumper/2,hbottom],
+        [-wbumper/2 + htop*tan(chamfertop),hbottom+htop],
+        [wbumper/2 - htop*tan(chamfertop),hbottom+htop],
+        [wbumper/2,hbottom],
+        [wbumper/2 - hbottom*tan(chamferbottom),0u"µm"],
+        [-wbumper/2 + hbottom*tan(chamferbottom),0u"µm"]
+    ]
+    nofillet = polycontour(verts)
+    filleted = polycontour(verts,fillet)
+    function(zcoord)
+        #I'd still like to fillet the top and bottom vertices on the 'inside'
+        filletinside = (zcoord < fillet) || (zcoord > (htop + hbottom - fillet))
+        #place a hatchline off of the contour to the left and at our desired height
+        hl=HatchLine([-wbumper,zcoord],[1,0]u"µm")
+        #our first coordinate is the first intersection of hl with nofillet near the beam
+        #attachments, filleted otherwise
+        firstpara = filletinside ?
+            sort(intersections(filleted,hl))[1] : sort(intersections(nofillet,hl))[1]
+        #our second coordinate is the second intersection of hl with filleted
+        secondpara = sort(intersections(filleted,hl))[2]
+        map([firstpara,secondpara]) do p
+            #convert to real world coordinates in Âµm
+            coords = pointalong(hl,p)
+            #we're interested in the x coordinate, add the dimensions on
+            coords[1] * u"µm"
+        end
+    end
+end
+
 
 function outlineverts(w,h1,h2,h3)
     #assuming the origin is at the center of the bottom edge
@@ -76,7 +190,7 @@ end
 
 function area(loc::LineOffsetContour)
     #this is the length of the line times the difference between the offsets
-    span(loc.edge)*1u"μm" * abs(x1-x2)
+    span(loc.edge)*1u"μm" * abs(loc.x1-loc.x2)
 end
 
 function getoffsetedges(loc::LineOffsetContour)
@@ -97,9 +211,9 @@ end
 
 function centroid(aoc::ArcOffsetContour)
     #total arc
-    alpha = aoc.stopangle - aoc.startangle
+    alpha = aoc.edge.stopangle - aoc.edge.startangle
     #make sure this is positive
-    alpha = aoc < 0 ? aoc + 2pi : aoc
+    alpha = alpha < 0 ? alpha + 2pi : alpha
 
     #get the edges of the contour
     edges = getoffsetedges(aoc)
@@ -109,24 +223,26 @@ function centroid(aoc::ArcOffsetContour)
     d = (2/3)*(sin(alpha)/alpha)*(r[2]^3 - r[1]^3)/(r[2]^2 - r[1]^2)
 
     #now get the direction
-    theta1 = aoc.startangle
-    theta2 = aos.stopangle
+    theta1 = aoc.edge.startangle
+    theta2 = aoc.edge.stopangle
     #theta2 needs to be greater than theta1
     theta2 = theta1 > theta2 ? theta2 + 2pi : theta2
 
     theta = mean([theta1,theta2])
-    [d*cos(theta), d*sin(theta)]
+    #this is the distance from the center of curvature
+    distance_from_center = [d*cos(theta), d*sin(theta)]
+    1u"μm"*aoc.edge.c + distance_from_center
 end
 
 function area(aoc::ArcOffsetContour)
     #total arc
-    alpha = aoc.stopangle - aoc.startangle
+    alpha = aoc.edge.stopangle - aoc.edge.startangle
     #make sure this is positive
-    alpha = aoc < 0 ? aoc + 2pi : aoc
+    alpha = alpha < 0 ? alpha + 2pi : alpha
 
     #get the edges of the contour
     edges = getoffsetedges(aoc)
-    r = sort([1u"µm"*e.r for e in edges])
+    r = sort([1u"μm"*e.r for e in edges])
     alpha*(r[2]^2 - r[1]^2)
 end
 
@@ -168,7 +284,7 @@ OffsetContour(edges::Vector{<:Tessen.Edge},x1,x2) = CompoundOffsetContour(edges,
 
 contours(coc::CompoundOffsetContour) = OffsetContour.(coc.edges,coc.x1,coc.x2)
 
-function area(aoc::CompoundOffsetContour)
+function area(coc::CompoundOffsetContour)
     sum(contours(coc)) do c
         area(c)
     end
@@ -186,22 +302,42 @@ function convert(T::Type{Contour}, coc::CompoundOffsetContour)
     c = contours(coc)
     firstedges = convert(Contour,c[1],endcap=false).edges
     lastedges = convert(Contour,c[end],startcap=false).edges
-    otheredges = map(convert.(Contour,c[2:end-1],startcap=false,endcap=false)) do c
+    otheredges::Vector{Vector{Tessen.Edge}}= map(convert.(Contour,c[2:end-1],startcap=false,endcap=false)) do c
         c.edges
     end
-    
-    Contour(vcat(firstedges,vcat(otheredges...),lastedges))
+    voe::Vector{Tessen.Edge} = vcat(otheredges...)
+    Contour(vcat(firstedges,voe,lastedges))
 end
 
 #add a span method for vectors of edges
-function Tessen.span(edges::Vector{Edge})
+function Tessen.span(edges::Vector{Tessen.Edge})
     sum(edges) do e
         span(e)
     end
 end
-#add a subsection method for vectors of edges
-function Tessen.subsection(edges::Vector{Edge},x1::Real,x2::Real)
-    @assert all([x1,x2] .< span(edges)) "Both x1 and x2 must be less than total span"
+#add a subsection method for vectors of edges. We will assume this corresponds to closed paths
+function Tessen.subsection(edges::Vector{Tessen.Edge},x1::Real,x2::Real)
+    #because this is a closed path we will accept x1 and x2 outside of the range 0 < x < span(edges)
+    @assert abs(x2 - x1) < span(edges) "can't make a subsection longer than the contour"
+    #if x1 is less than 0, figure out a corresponding positive position
+    while x1 < 0
+        x1 += span(edges)
+        #keep x1 and x2 in sync
+        x2 += span(edges)
+    end
+    #also make sure x1 is less than span(edges)
+    while x1 > span(edges)
+        x1 -= span(edges)
+        x2 -= span(edges)
+    end
+    #now if x2 > span(edges) we need to subsection twice to stitch over the origin
+    if x2 > span(edges)
+        #how much to we go over the end
+        overhang = x2 - span(edges)
+        return vcat(subsection(edges,x1,span(edges)),subsection(edges,0,overhang))
+    end
+    #otherwise we're within the bounds 0 < x < span(edges)
+    @assert all([x1,x2] .<= span(edges)) "Both x1 and x2 must be less than total span"
     #make a local copy of edges so we don't fuck with our caller
     edges = copy(edges)
     #find our starting place
@@ -222,7 +358,7 @@ function Tessen.subsection(edges::Vector{Edge},x1::Real,x2::Real)
     edges[1] = subsection(edges[1],distuntilstart,span(edges[1]))
     distuntilend -= distuntilstart
     #now we just add sections to the output until we have enough distance
-    output = Edge[]
+    output = Tessen.Edge[]
     for e in edges
         #does this edge get us all the way to the end?
         if span(e) > distuntilend
@@ -236,6 +372,99 @@ function Tessen.subsection(edges::Vector{Edge},x1::Real,x2::Real)
         end
     end
     return output
+end
+
+
+#==============We don't actually need this===========================
+"""
+```julia
+rotatestart(edges,x)
+```
+Modify a `Vector{Edge}` to represent the same geometry, but with the
+startpoint of the (assumed closed) contour slid along the path by `x`
+"""
+function rotatestart(edges::Vector{Tessen.Edge},x::Quantity)
+    #copy input so we don't mess up our caller
+    edges = copy(edges)
+    disttomove = ustrip(u"μm",x)
+    #copy sections to the end until we hit disttomove
+    while true
+        #can we get through disttomove with the first segment in edges
+        if disttomove < span(edges[1])
+            #cut off disttomove from edges[1] and move to the back
+            newend = subsection(edges[1],0,disttomove)
+            push!(edges,newend)
+            #modify what's left on the front
+            edges[1] = subsection(edges[1],disttomove,span(edges[1]))
+            #done
+            break
+        else
+            #move the whole segment to the back
+            newend = popfirst!(edges)
+            push!(edges,newend)
+            #update how far we have to go
+            disttomove -= span(newend)
+        end
+    end
+    return edges
+end
+=====================================================================#
+
+function bumper(path,profile,maxseglength,zstart,zend,dslice,overlap,cutangle)
+    #how much space per segment is taken up by the chamfered overlap zones
+    cutdist_per_seg = 2*(zend-zstart)*tan(cutangle)
+    #max length of one crossection
+    max_xlength = maxseglength - cutdist_per_seg - 2overlap
+    #number of segments needed to keep things short enough
+    numsegs = ceil(Int,1u"μm"*span(path)/maxseglength)
+    #actual length of one crossection
+    xlength = 1u"μm"*span(path)/numsegs
+    #create a vector of vectors to store the slices for each block in
+    bottomblocks = map(1:numsegs) do _
+        Pair[]
+    end
+    topblocks = deepcopy(bottomblocks)
+    zhalf = mean([zstart,zend])
+    #build the first set of slices
+    curpos = 0u"μm"
+    #we will print two layers of blocks
+    for (start,stop,blocks) in [(zstart,zhalf + overlap/2,bottomblocks),(zhalf-overlap/2,zend,topblocks)]
+        firstsliceoffset = 0u"μm"
+        for z in start:dslice:stop
+            for i in 1:numsegs
+                #if this is the first segment we have to chamfer for overlap on both sides
+                startoffset = i == 1 ? firstsliceoffset : zero(firstsliceoffset)
+                #if this is the last segment we need to chamfer the end in the opposite direction
+                #this should be the same as the firstsliceoffset
+                endoffset = i == numsegs ? firstsliceoffset : zero(firstsliceoffset)
+                thisseg = subsection(path,curpos + startoffset,curpos + xlength + endoffset + overlap)
+                curpos += xlength
+                #now we make an offsetcontour based on `profile`
+                thiscontour = OffsetContour(thisseg,profile(z)...)
+                push!(blocks[i], z => thiscontour)
+            end
+            #now we'll move up by dslice, so we should slide over a bit to make our cutangle
+            curpos -= dslice*tan(cutangle)
+            #also update firstsliceoffset, need to do twice as much since we're updating curpos
+            firstsliceoffset += 2*dslice*tan(cutangle)
+        end
+        #move curpos so the seams of our blocks don't overlap
+        curpos += xlength/2
+    end
+    blockcontours = vcat(bottomblocks,topblocks)
+    #turn these into blocks
+    blocks = map(blockcontours) do bc
+        midcontour = bc[floor(Int,length(bc)/2)][2]
+        slices = map(bc) do (z,bci)
+            z => Slice([convert(Contour,bci)])
+        end
+        thisblock = Block(slices...)
+        #move the origin of this block to the centroid of the center slice
+        bt = translateorigin(thisblock,centroid(midcontour))
+        hatch(bt,dhatch=1u"μm",bottomdir=pi/4,diroffset=pi)
+    end
+    #build a superblock
+    SuperBlock(blocks...)
 end
 
 end # module Toast
